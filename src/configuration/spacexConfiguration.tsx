1bbf4e67c081091a29447a8cacfc259f8addf2d7
sharoni <sharoni.royc@gmail.com>
2023-04-16 00:28:43 -0400

    Added configuration file to be used across project

---
+++ src/configuration/spacexConfiguration.tsx
@@ -0,0 +1,4 @@
+// configuration file. Used across the application where needed.
+export const defaultImageUri = "https://images2.imgbox.com/48/a8/LTqq8OrE_o.png"
+export const spacexAPI = "https://api.spacexdata.com/v5"
+export const getLaunches = "/launches"
