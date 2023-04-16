603b0c5eb4c18f58fb813de981aff83a6cfc629a
sharoni <sharoni.royc@gmail.com>
2023-04-16 00:40:02 -0400

    Styling changes on navigation bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import RocketInfoScreen from './src/screens/RocketInfoScreen';
-import { Launch } from './src/services/models';
+import { Launch } from './src/model/models';
 import AboutMeScreen from './src/screens/AboutMeScreen';
 
 export type RocketDetailStackParam = {
@@ -12,20 +11,12 @@ export type RocketDetailStackParam = {
   AboutMe: undefined
 };
 
-const DetailsScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
 const Stack = createNativeStackNavigator<RocketDetailStackParam>();
 export default function App() {
   return (
       <NavigationContainer>
           <Stack.Navigator initialRouteName="Home" screenOptions={()=>({
-            headerStyle: {backgroundColor: 'mediumturquoise'}
+            headerStyle: {backgroundColor: '#87ceeb'}, headerTitleStyle: {color: '#191970', fontWeight: 'bold'} , title: 'SpaceX - Launches'
           })}>
             <Stack.Screen name ="Home" component={HomeScreen}/>
             <Stack.Screen name="RocketInfo" component={RocketInfoScreen}/>
@@ -34,12 +25,3 @@ export default function App() {
       </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
